e186dc2d152f3d4c5bb483b05ae7dbd3a25cde6c
Neerthigan Sivananthan <nsivananthan@gobasports.com>
2018-05-12 19:23:39 -0400

    Removed hotkey code

--- ocr_v2.py
+++ ocr_v2.py
@@ -7,40 +7,11 @@ pytesseract.pytesseract.tesseract_cmd = 'C:/Program Files/Tesseract-OCR/tesserac
 counter = True
 boughtPrice = 102.38
 
-screenWidth, screenHeight = gui.size()
-qtyPos = {'x': screenWidth * .2885, 'y': screenHeight * .1805}
-orderTypePos = {'x': screenWidth * .3380, 'y': screenHeight * .1796}
-# After first option the height difference for each option is 12
-orderTypeOptionsPos = {
-    'market': {'y': orderTypePos['y'] + 25},
-    'limit': {'y': orderTypePos['y'] + 37},
-    'stop': {'y': orderTypePos['y'] + 49},
-    'stop_lmt': {'y': orderTypePos['y'] + 61}
-}
-limitPos = {'x': screenWidth * .4052, 'y': screenHeight * .1777}
-buyPos = {'x': screenWidth * .6406, 'y': screenHeight * .1796}
-
-
-def hotkey():
-    gui.click(qtyPos['x'], qtyPos['y'], 3, interval=0.25, button='left')
-    gui.typewrite('1000')
-
-    gui.click(orderTypePos['x'], orderTypePos['y'], 1, button='left')
-    gui.click(orderTypePos['x'], orderTypeOptionsPos['limit']
-              ['y'], button='left', clicks=3, interval=0.25)
-
-    gui.click(limitPos['x'], limitPos['y'], 3, interval=0.25, button='left')
-    gui.typewrite('200')
-
-    gui.click(buyPos['x'], buyPos['y'], 1, button='left')
-
-
 while(counter):
     price = float(pytesseract.image_to_string(gui.screenshot(
         region=(135, 595, 80, 25)), config="-psm 6"))
     if (price - boughtPrice >= .10):
         counter = False
-        # hotkey()
     print("Curr price: ", price)
 
 print("Sold Price ", price)
